86d70a454d69b2088b429e119734cea61e329790
NavaneethanJeyaprakash <navaneethan.jeyaprakash@datalensai.com>
2024-08-17 12:41:38 +0530

    logger & exception file committed.

--- get_data.py
+++ get_data.py
@@ -1,11 +1,14 @@
 import json, os, sys
 from dotenv import load_dotnev
 
-import certifi, pandas as pd, numpy as np, pymoongo
+import certifi, pandas as pd, numpy as np, pymongo
 
 from networksecurity.exception.exception import NetworkSecurityExecption
 from networksecurity.logger.logger import logging
 
+load_dotnev()
+certifi.where()
+mongo_client = os.getenv("MONGO_DB_URL")
 
 class NetworkDataExtraction:
     def __init__(self) -> None:
@@ -15,7 +18,7 @@ class NetworkDataExtraction:
         except Exception as e:
             raise NetworkSecurityExecption(e, sys)
 
-    def csv_to_json_convertor(self):
+    def csv_tojson_convertor(self):
         try: 
             pass
         except Exception as e:
